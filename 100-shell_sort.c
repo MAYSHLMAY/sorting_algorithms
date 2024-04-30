822f810afc4aa58501123938f92bdeada39bfb10
Mikiyas <educationforlife424@gmail.com>
2024-05-01 02:07:37 +0300

    Shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,32 +7,32 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t spac = 1;
-    size_t outerIndex, innerIndex;
-    int temp;
+	size_t spac = 1;
+	size_t outerIndex, innerIndex;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (spac < size / 3)
-        spac = spac * 3 + 1;
+	while (spac < size / 3)
+		spac = spac * 3 + 1;
 
-    while (spac >= 1)
-    {
-        outerIndex = spac;
-        while (outerIndex < size)
-        {
-            temp = array[outerIndex];
-            innerIndex = outerIndex;
-            while (innerIndex >= spac && array[innerIndex - spac] > temp)
-            {
-                array[innerIndex] = array[innerIndex - spac];
-                innerIndex -= spac;
-            }
-            array[innerIndex] = temp;
-            outerIndex++;
-        }
-        spac /= 3;
-        print_array(array, size);
-    }
+	while (spac >= 1)
+	{
+		outerIndex = spac;
+		while (outerIndex < size)
+		{
+			temp = array[outerIndex];
+			innerIndex = outerIndex;
+			while (innerIndex >= spac && array[innerIndex - spac] > temp)
+			{
+				array[innerIndex] = array[innerIndex - spac];
+				innerIndex -= spac;
+			}
+			array[innerIndex] = temp;
+			outerIndex++;
+		}
+		spac /= 3;
+		print_array(array, size);
+	}
 }

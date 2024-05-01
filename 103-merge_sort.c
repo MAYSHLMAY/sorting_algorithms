2cdf32b6cd99bd46b844d7b780a5ac08d95d806d
Hizkeal <hizkealyb17@gmail.com>
2024-05-01 03:40:47 +0300

    Merge sort

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -5,51 +5,51 @@
 /**
  * m_h - merge sort uppp
  * @array: array to be sort
- * @tp: to arr for holding sorted elements
+ * @temp_array: to arr for holding sorted elements
  * @size: size of the array
  */
 void m_h(int *array, int *temp_array, size_t size)
 {
-    size_t half_size = size / 2;
-    size_t left_index = 0, right_index = 0, merged_index;
+	size_t h_s = size / 2;
+	size_t l_i = 0, r_i = 0, merged_index;
 
-    if (size < 2)
-        return;
+	if (size < 2)
+		return;
 
-    m_h(array, temp_array, half_size);
-    m_h(array + half_size, temp_array + half_size, size - half_size);
+	m_h(array, temp_array, h_s);
+	m_h(array + h_s, temp_array + h_s, size - h_s);
 
-    printf("%s", "Merging...\n");
-    printf("%s", "[left]: ");
-    print_array(array, half_size);
-    printf("%s", "[right]: ");
-    print_array(array + half_size, size - half_size);
+	printf("%s", "Merging...\n");
+	printf("%s", "[left]: ");
+	print_array(array, h_s);
+	printf("%s", "[right]: ");
+	print_array(array + h_s, size - h_s);
 
-    merged_index = 0;
-    while (merged_index < size)
-    {
-        if (right_index >= size - half_size || (left_index < half_size && array[left_index] < (array + half_size)[right_index]))
-        {
-            temp_array[merged_index] = array[left_index];
-            left_index++;
-        }
-        else
-        {
-            temp_array[merged_index] = (array + half_size)[right_index];
-            right_index++;
-        }
-        merged_index++;
-    }
+	merged_index = 0;
+	while (merged_index < size)
+	{
+		if (r_i >= size - h_s || (l_i < h_s && array[l_i] < (array + h_s)[r_i]))
+		{
+			temp_array[merged_index] = array[l_i];
+			l_i++;
+		}
+		else
+		{
+			temp_array[merged_index] = (array + h_s)[r_i];
+			r_i++;
+		}
+		merged_index++;
+	}
 
-    merged_index = 0;
-    while (merged_index < size)
-    {
-        array[merged_index] = temp_array[merged_index];
-        merged_index++;
-    }
+	merged_index = 0;
+	while (merged_index < size)
+	{
+		array[merged_index] = temp_array[merged_index];
+		merged_index++;
+	}
 
-    printf("%s", "[Done]: ");
-    print_array(array, size);
+	printf("%s", "[Done]: ");
+	print_array(array, size);
 }
 
 /**
